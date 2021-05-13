81023b493167940f9c20c298790616bb393af15e
Jun_Hyeok_Choi <lluxuryl98@naver.com>
2021-05-14 00:47:35 +0900

    시간 관련 출력
    현재 시간, 설정 시간, 해가 뜰 때까지의 시간을 메인화면에 추가함.

--- src/GUI/MainGUI.py
+++ src/GUI/MainGUI.py
@@ -1,6 +1,9 @@
 from tkinter import *
+from tkinter import font
 from PIL import Image, ImageTk
+import datetime
 
+T_POS_X, T_POS_Y = 100, 150
 B_Size = 80
 B_POS_X, B_POS_Y = 1100, 100
 
@@ -10,20 +13,55 @@ class MainGUI:
         self.gui.title("해가 뜰 때까지")
         self.gui.geometry("1280x720")
         self.gui.resizable(width=False, height=False)
+        self.canvas = Canvas(self.gui, height=720, width=1280)
+        self.canvas.pack()
 
-        self.LoadBackGround()
-        self.LoadButtons()
+        self.LoadFonts()
+        self.SetBackGround()
+        self.SetMainTexts()
+        self.SetButtons()
 
         self.gui.mainloop()
 
-    def LoadBackGround(self):
+    def LoadFonts(self):
+        self.font_dic = {}
+        print(list(font.families()))
+        self.font_dic["Forte"] = font.Font(family="Forte", size=30, weight="bold")
+        self.font_dic["Cooper"] = font.Font(family="Cooper Black", size=16, weight="bold")
+
+    def SetBackGround(self):
         self.bgImg = PhotoImage(file="asset/bg_sunset.png")
-        self.bg_label = Label(self.gui, image=self.bgImg)
-        self.bg_label.place(x=-2, y=-2)
+        self.canvas.create_image((0, 0), image=self.bgImg, anchor=N+W)
 
-        pass
+    def SetMainTexts(self):
+        self.travel_date = datetime.datetime(2021, 5, 15, 20, 30)
+        self.cur_location = "서울"
 
-    def LoadButtons(self):
+        self.subtitle = self.canvas.create_text((T_POS_X, T_POS_Y), font=self.font_dic["Forte"],\
+                                                text="해가 뜰 때까지", anchor=W)
+        now = datetime.datetime.now()
+        time_remaining = self.travel_date - now
+        self.time_remaing_text = self.canvas.create_text((T_POS_X, T_POS_Y + 80), font=self.font_dic["Forte"],\
+                                text=str(time_remaining), anchor=W)
+        self.cur_time_text = self.canvas.create_text((T_POS_X, T_POS_Y + 180), font=self.font_dic["Cooper"],\
+                                text=("현재 시간: " + now.strftime("%Y-%m-%d %H:%M:%S")), anchor=W)
+        self.travel_date_text = self.canvas.create_text((T_POS_X, T_POS_Y + 230), font=self.font_dic["Cooper"],\
+                                text=("일출 시간: " + self.travel_date.strftime("%Y-%m-%d %H:%M:%S")), anchor=W)
+        self.cur_location_text = self.canvas.create_text((T_POS_X, T_POS_Y + 280), font=self.font_dic["Cooper"],\
+                                text=("지역: " + self.cur_location), anchor=W)
+        self.UpdateTime()
+
+    def UpdateTime(self):
+        now = datetime.datetime.now()
+        time_remaining = self.travel_date - now
+        self.canvas.itemconfig(self.time_remaing_text, text=str("{0} Day {1} Hours {2} Minutes {3} Seconds")\
+                              .format(time_remaining.days, time_remaining.seconds // 3600,\
+                                      time_remaining.seconds % 3600 // 60, time_remaining.seconds % 60))
+        self.canvas.itemconfig(self.cur_time_text, text=("현재 시간: " + now.strftime("%Y-%m-%d %H:%M:%S")))
+        self.canvas.itemconfig(self.travel_date_text, text=("일출 시간: " + self.travel_date.strftime("%Y-%m-%d %H:%M:%S")))
+        self.gui.after(500, self.UpdateTime)
+
+    def SetButtons(self):
         self.b_image_list = []
         self.SetMainButton("asset/magnifier.png", B_POS_X, B_POS_Y)
         self.SetMainButton("asset/map.png", B_POS_X, B_POS_Y + B_Size)
@@ -39,4 +77,6 @@ class MainGUI:
         img = img.resize((B_Size, B_Size), Image.ANTIALIAS)
         b_image = ImageTk.PhotoImage(img)
         self.b_image_list.append(b_image)
-        Button(self.gui, image=b_image, width=B_Size, height=B_Size).place(x=x_pos, y=y_pos)
+        Button(self.canvas, image=b_image, width=B_Size, height=B_Size).place(x=x_pos, y=y_pos)
+
+
