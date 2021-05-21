3fe29b1981b004c1675c16071fbe0cf9b4212e6a
undugy <70964651+undugy@users.noreply.github.com>
2021-05-21 20:20:56 +0900

    suninfo class화

--- src/SunInfo.py
+++ src/SunInfo.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import urllib.request
 from xml.dom.minidom import parse,parseString
 
-Sun_infoDoc=None
+
 
 #인증키
 encoding_key="yc4yEDg1e6WWu78efmKyu3cUfYRodEuVtAKv%2FpABsgm0CvRvkx9RHCMAPXsx9v7mU6Fg7XMvq3zTBq%2BvB6ePhQ%3D%3D"
@@ -15,75 +15,80 @@ server="http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAre
 
 
 
+class Suninfo:
+    CategoryDict = {"날짜": "locdata", "지역": "location", "경도": "longitude",
+                    "경도(10진수)": "longitudeNum", "위도": "latitude", "위도(10진수)": "latitudeNum",
+                    "일출": "sunrise", "일중": "suntransit", "일몰": "sunset", "월출": "moonrise",
+                    "월중": "moontransit", "월몰": "moonset", "시민박명(아침)": "civilm", "시민박명(저녁)": "civile",
+                    "항해박명(아침)": "nautm", "항해박명(저녁)": "naute", "천문박명(아침)": "astm", "천문박명(저녁)": "aste"
+                    }
+    def __init__(self,location="서울",locdata="20210515"):
+        self.InitSunData(location,locdata)
+
+    def InitSunData(self,location,locdata):
+
+        my_location = urllib.parse.quote(location)
+        location = "&location=" + my_location
+
+        # 날짜
+        locdate = "&locdate=" + locdata
+
+        url=server+encoding_key+locdate+location
+        request=urllib.request.Request(url)
+
+        response=urllib.request.urlopen(request)
+        rescode=response.getcode()
+
+        if(rescode==200):
+            response_body=response.read()
+            a=response_body.decode("utf-8")
+            self.SunInfoDoc=parseString(a)
+
+        else:
+            print("Error Code:"+rescode)
 
 
+    def checkDocument(self):
 
-def InitData(location,locdata):
-    global Sun_infoDoc
-    # 지역
-    my_location = urllib.parse.quote(location)
-    location = "&location=" + my_location
+        if self.SunInfoDoc == None:
+            print("Error : Document is empty")
+            return False
+        return True
 
-    # 날짜
-    locdate = "&locdate=" + locdata
+    #정보 마다로 수정해야함
+    def SearchSunData(self,category):
 
-    url=server+encoding_key+locdate+location
-    request=urllib.request.Request(url)
+        if not self.checkDocument():
+            return None
 
-    response=urllib.request.urlopen(request)
-    rescode=response.getcode()
+        try:
+            tree = ET.fromstring(str(self.SunInfoDoc.toxml()))
+        except Exception:
+            print("Element Tree parsing Error : maybe the xml document is not corrected.")
+            return None
 
-    if(rescode==200):
-        response_body=response.read()
-        a=response_body.decode("utf-8")
-        Sun_infoDoc=parseString(a)
+        SunElements = tree.iter("item")
+        for item in SunElements:
+            strTitle = item.find(category)
 
-    else:
-        print("Error Code:"+rescode)
+        return strTitle.text
 
 
-def checkDocument():
-    global Sun_infoDoc
-    if Sun_infoDoc == None:
-        print("Error : Document is empty")
-        return False
-    return True
-
-#정보 마다로 수정해야함
-def SearchSunData(category):
-    global Sun_infoDoc
-    if not checkDocument():
-        return None
-
-    try:
-        tree = ET.fromstring(str(Sun_infoDoc.toxml()))
-    except Exception:
-        print("Element Tree parsing Error : maybe the xml document is not corrected.")
-        return None
-
-    SunElements = tree.iter("item")
-    for item in SunElements:
-        strTitle = item.find(category)
-
-    return strTitle.text
-
-InitData("서울","20210515")
-'''
-나중에 태양고도용
-def SearchSunData(a,b):
-    global SuninfoDoc
-    if not checkDocument():
-        return None
-
-    try:
-        tree = ET.fromstring(str(SuninfoDoc.toxml()))
-    except Exception:
-        print("Element Tree parsing Error : maybe the xml document is not corrected.")
-        return None
-
-    SunElements = tree.iter(a)
-    for item in SunElements:
-        strTitle = item.find(b)
-
-    return strTitle
-'''
+    '''
+    def SearchSunHeightData(self,a,b):
+        global SuninfoDoc
+        if not checkDocument():
+            return None
+    
+        try:
+            tree = ET.fromstring(str(SuninfoDoc.toxml()))
+        except Exception:
+            print("Element Tree parsing Error : maybe the xml document is not corrected.")
+            return None
+    
+        SunElements = tree.iter(a)
+        for item in SunElements:
+            strTitle = item.find(b)
+    
+        return strTitle
+    '''
