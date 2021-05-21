ba72d94aff3b7bfb241801b6c27123cc8d904cf3
undugy <70964651+undugy@users.noreply.github.com>
2021-05-21 20:24:29 +0900

    Update Calendar.py

--- src/Calendar.py
+++ src/Calendar.py
@@ -7,7 +7,7 @@ import datetime
 class CalendarGUI:
     is_open = False
     #새로 생성되는 클래스의 인자에 MainGUI을 넣어서 MainGUI 객체를 가져오게 함 그렇게 되면 MainGUI에 있는 변수들을 수정 가능
-    def __init__(self, MainGui):
+    def __init__(self, MainGui,is_travel):
         if CalendarGUI.is_open:
             return
         CalendarGUI.is_open = True
@@ -28,9 +28,10 @@ class CalendarGUI:
     #현재는 시간이 20, 30으로 고정되어 있는데 이 부분은 xml 데이터 파일의 정보를 가져와서 설정해 주어야함.
     def SetTravelDate(self):
         date = self.cal.selection_get()
+        #여기서 xml 데이터 가져온다.
         self.main_gui.travel_date = datetime.datetime(date.year, date.month, date.day, 20, 30)
-        self.main_gui.text.set(self.main_gui.travel_date.strftime("%Y-%m-%d"))
         print(self.main_gui.travel_date)
+
 
     def Closing(self):
         CalendarGUI.is_open = False
