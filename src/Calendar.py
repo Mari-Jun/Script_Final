75c87c070a2b83559dc61d99540731194fe83ead
Jun_Hyeok_Choi <lluxuryl98@naver.com>
2021-05-25 01:38:01 +0900

    Calendar Update
    Main에서 로드 된 xml 파일을 얻어오기 위해 Calendar를 손봤음

--- src/Calendar.py
+++ src/Calendar.py
@@ -20,28 +20,18 @@ class CalendarGUI:
         self.cal = Calendar(self.gui, selectmode='day', year=now.year, month=now.month, day=now.day)
         self.cal.pack(pady=10)
 
-        Button(self.gui, text="날짜 선택", command=lambda cmd=selectCommand: self.SetTravelDate(cmd)).pack()
+        Button(self.gui, text="날짜 선택", command=lambda cmd=selectCommand: self.SetTravelDate(command=cmd)).pack()
 
         self.gui.protocol("WM_DELETE_WINDOW", self.Closing)
 
         self.gui.mainloop()
 
-    def PrintHello(self):
-        print("Hello")
-
     #현재는 시간이 20, 30으로 고정되어 있는데 이 부분은 xml 데이터 파일의 정보를 가져와서 설정해 주어야함.
     def SetTravelDate(self, command=None):
         date = self.cal.selection_get()
-        #여기서 xml 데이터 가져온다.
-        self.sun_info=SunInfo.Suninfo("서울",date.strftime("%Y%m%d"))
-        self.sun_time=self.sun_info.LoadTimes(self.sun_info.CategoryDict["일출"])
-        self.main_gui.travel_date = datetime.datetime(date.year, date.month, date.day, self.sun_time[0],self.sun_time[1])
 
         if command is not None:
             command(date)
-
-        print(self.main_gui.travel_date)
-
 
     def Closing(self):
         CalendarGUI.is_open = False
