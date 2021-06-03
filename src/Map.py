430ea67ec9325ccff63afce3aa33e6516cd1a6ad
Jun_Hyeok_Choi <lluxuryl98@naver.com>
2021-06-04 01:11:55 +0900

    Map오류 수정
    cefpython에 올라와있는 tkinter 예제를 보고 했더니 버그가 존재해서 교수님 코드를 보고 수정함

--- src/Map.py
+++ src/Map.py
@@ -2,6 +2,8 @@ from cefpython3 import cefpython as cef
 from tkinter import *
 import platform
 import os
+import sys
+import threading
 from bs4 import BeautifulSoup
 
 # Platforms
@@ -41,16 +43,25 @@ class MapGUI:
         self.gui.geometry("{width}x{height}".format(width=GUI_WIDTH, height=GUI_HEIGHT))
         self.gui.resizable(width=False, height=False)
 
-        self.browser = BrowserFrame(self.gui, self.main_gui)
-        self.browser.grid(row=1, column=0, sticky=(N + S + E + W))
+        self.frame = Frame(self.gui, width=GUI_WIDTH, height=GUI_HEIGHT)
+        self.frame.grid(row=1, column=0, sticky=(N + S + E + W))
         Grid.rowconfigure(self.gui, 1, weight=1)
         Grid.columnconfigure(self.gui, 0, weight=1)
 
-        cef.Initialize()
-        self.browser.mainloop()
-        cef.Shutdown()
+        thread = threading.Thread(target=self.embed_broser)
+        thread.daemon = True
+        thread.start()
 
         self.gui.mainloop()
+
+    def embed_broser(self):
+        sys.excepthook = cef.ExceptHook
+        window_info = cef.WindowInfo(self.frame.winfo_id())
+        rect = [0, 0, GUI_WIDTH, GUI_HEIGHT]
+        window_info.SetAsChild(self.frame.winfo_id(), rect)
+        cef.Initialize()
+        browser = cef.CreateBrowserSync(window_info, url=MAP_HTMP_PATH)
+        cef.MessageLoop()
 
 
 class BrowserFrame(Frame):
@@ -62,29 +73,6 @@ class BrowserFrame(Frame):
         self.main_gui = MainGUI
         self.bind("<Configure>", self.on_configure)
         """For focus problems see Issue #255 and Issue #535. """
-
-        self.UpdateInfo()
-
-    def embed_browser(self):
-        window_info = cef.WindowInfo()
-        rect = [0, 0, self.winfo_width(), self.winfo_height()]
-        window_info.SetAsChild(self.winfo_id(), rect)
-        self.browser = cef.CreateBrowserSync(window_info, url=MAP_HTMP_PATH)
-        self.message_loop_work()
-
-    def message_loop_work(self):
-        cef.MessageLoopWork()
-        self.after(10, self.message_loop_work)
-
-    def UpdateInfo(self):
-        if self.browser is not None:
-
-            self.myvisitor = Visitor(self.main_gui)
-            mainFrame = self.browser.GetMainFrame()
-            mainFrame.GetSource(self.myvisitor)
-
-        self.after(1000, self.UpdateInfo)
-
 
     def on_configure(self, _):
         self.embed_browser()
