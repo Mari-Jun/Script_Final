962df4e69a60627d92fec5a585f55e41f3cb1aae
undugy <70964651+undugy@users.noreply.github.com>
2021-06-04 18:19:40 +0900

    텔레그램 채팅완료 함수수정

--- src/teller.py
+++ src/teller.py
@@ -11,82 +11,100 @@ from bs4 import BeautifulSoup
 import re
 from datetime import date, datetime, timedelta
 import traceback
-
+from SunInfo import *
 import noti
 
 
-def replyAptData(date_param, user, loc_param='서울'):
-    print(user, date_param, loc_param)
-    res_list = noti.getData( loc_param, date_param )
-    msg = ''
-    for r in res_list:
-        print( str(datetime.now()).split('.')[0], r )
-        if len(r+msg)+1>noti.MAX_MSG_LENGTH:
+
+
+class TelegramBot:
+    def __init__(self):
+
+        today = date.today()
+        self.travel_date = today
+
+
+
+        bot = telepot.Bot(noti.TOKEN)
+        pprint(bot.getMe())
+
+        bot.message_loop(self.handle)
+
+
+
+        while 1:
+            time.sleep(10)
+    def makeSuninfo(self,loc_param,time):
+        self.suninfo = Suninfo(loc_param, time)
+        longitude = self.suninfo.LoadLongitude()
+        latitude = self.suninfo.LoadLatitude()
+        tw_civil = self.suninfo.LoadTimes(Suninfo.CategoryDict["시민박명(아침)"]) + self.suninfo.LoadTimes(
+            Suninfo.CategoryDict["시민박명(저녁)"])
+        tw_naut = self.suninfo.LoadTimes(Suninfo.CategoryDict["항해박명(아침)"]) + self.suninfo.LoadTimes(
+            Suninfo.CategoryDict["항해박명(저녁)"])
+        tw_ast = self.suninfo.LoadTimes(Suninfo.CategoryDict["천문박명(아침)"]) + self.suninfo.LoadTimes(
+            Suninfo.CategoryDict["천문박명(저녁)"])
+
+        time1 = self.suninfo.LoadTimes(Suninfo.CategoryDict["일출"])
+        time2 = self.suninfo.LoadTimes(Suninfo.CategoryDict["일중"])
+        time3 = self.suninfo.LoadTimes(Suninfo.CategoryDict["일몰"])
+
+        msg = ''' 
+
+                        날짜 : {0} , 지역 : {1}
+                        위치: 동경 {2}도{3}분 / 북위 {4}도{5}분
+
+                        박명시간
+                        시민박명(아침/저녁) :  아침-{6}시{7}분  /  저녁- {8}시{9}분
+                        항해박명(아침/저녁) : 아침-{10}시{11}분  /  저녁- {12}시{13}분
+                        천문박명(아침/저녁) : 아침-{14}시{15}분  /  저녁- {16}시{17}분
+
+                        해뜨는 시각(일출) : {18}시 {19}분
+                        한낮의 시각(남중) : {20}시 {21}분
+                        해지는 시각(일몰) : {22}시 {23}분
+
+                    '''.format(time[:4]+"/"+time[4:6]+"/"+time[6:], loc_param, longitude[0], longitude[1],
+                               latitude[0], latitude[1], tw_civil[0], tw_civil[1], tw_civil[2], tw_civil[3],
+                               tw_naut[0], tw_naut[1], tw_naut[2], tw_naut[3], tw_ast[0], tw_ast[1], tw_ast[2],
+                               tw_ast[3],
+                               time1[0], time1[1], time2[0], time2[1], time3[0], time3[1])
+        return msg
+    def replyTodayData(self,date_param, user, loc_param='서울'):
+
+
+        msg=self.makeSuninfo(loc_param,date_param)
+        if msg:
             noti.sendMessage( user, msg )
-            msg = r+'\n'
         else:
-            msg += r+'\n'
-    if msg:
-        noti.sendMessage( user, msg )
-    else:
-        noti.sendMessage( user, '%s 기간에 해당하는 데이터가 없습니다.'%date_param )
+            noti.sendMessage( user, '%s 기간에 해당하는 데이터가 없습니다.'%date_param )
 
-def save( user, loc_param ):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
-    try:
-        cursor.execute('INSERT INTO users(user, location) VALUES ("%s", "%s")' % (user, loc_param))
-    except sqlite3.IntegrityError:
-        noti.sendMessage( user, '이미 해당 정보가 저장되어 있습니다.' )
-        return
-    else:
-        noti.sendMessage( user, '저장되었습니다.' )
-        conn.commit()
+    def replyDayPlaceData(self, user,loc_param ,date_param ):
 
-def check( user ):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
-    cursor.execute('SELECT * from users WHERE user="%s"' % user)
-    for data in cursor.fetchall():
-        row = 'id:' + str(data[0]) + ', location:' + data[1]
-        noti.sendMessage( user, row )
+            msg=self.makeSuninfo(loc_param,date_param)
+            if msg:
+                noti.sendMessage(user, msg)
+            else:
+                noti.sendMessage(user, '%s 기간에 해당하는 데이터가 없습니다.' % date_param)
 
 
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    if content_type != 'text':
-        noti.sendMessage(chat_id, '난 텍스트 이외의 메시지는 처리하지 못해요.')
-        return
+    def handle(self,msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type != 'text':
+            noti.sendMessage(chat_id, '난 텍스트 이외의 메시지는 처리하지 못해요.')
+            return
 
-    text = msg['text']
-    args = text.split(' ')
+        text = msg['text']
+        args = text.split(' ')
 
-    if text.startswith('지역') and len(args)>1:
-        print('try to 지역', args[1])
-        replyAptData( '20170501', chat_id, args[1] )
-    elif text.startswith('저장')  and len(args)>1:
-        print('try to 저장', args[1])
-        save( chat_id, args[1] )
-    elif text.startswith('확인'):
-        print('try to 확인')
-        check( chat_id )
-    else:
-        noti.sendMessage(chat_id, '모르는 명령어입니다.\n지역 [지역번호], 저장 [지역번호], 확인 중 하나의 명령을 입력하세요.')
+        if text.startswith('지역은') and len(args)>1:
+            print('try to 지역', args[1])
+            self.replyTodayData( self.travel_date.strftime("%Y%m%d"), chat_id, args[1] )
+        elif text.startswith('여기이때')  and len(args)>2:
+            print('try to 저장', args[1])
+            self.replyDayPlaceData( chat_id, args[1],args[2])
+
+        else:
+            noti.sendMessage(chat_id, '모르는 명령어입니다.\n지역은 [지역이름], 여기이때 [지역이름] [날짜] 중 하나의 명령을 입력하세요.')
 
 
-today = date.today()
-current_month = today.strftime('%Y%m')
-
-print( '[',today,']received token :', noti.TOKEN )
-
-bot = telepot.Bot(noti.TOKEN)
-pprint( bot.getMe() )
-
-bot.message_loop(handle)
-
-print('Listening...')
-
-while 1:
-  time.sleep(10)
+TelegramBot()
